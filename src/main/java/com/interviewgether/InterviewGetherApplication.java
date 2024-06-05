ff147bad479901735308ec4f03d3377856c2e93e
avhhust <avgystunpolek@gmail.com>
2024-06-05 11:38:57 -0500

    Spring Boot initial stage

---
+++ src/main/java/com/interviewgether/InterviewGetherApplication.java
@@ -0,0 +1,13 @@
+package com.interviewgether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewGetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewGetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviewgether/InterviewGetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewgether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewGetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
