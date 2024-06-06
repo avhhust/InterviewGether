1ecb84575229fefaaebde956a4fb9f15177c41e3
avhhust <avgystunpolek@gmail.com>
2024-06-06 18:33:51 -0500

    Added RoleService and implemented RoleServiceImpl for accessing Role data from DB

---
+++ src/main/java/com/interviewgether/service/implementation/RoleServiceImpl.java
@@ -0,0 +1,61 @@
+package com.interviewgether.service.implementation;
+
+
+import com.interviewgether.model.Role;
+import com.interviewgether.repository.RoleRepository;
+import com.interviewgether.service.RoleService;
+import com.interviewgether.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+    private final UserService userService;
+
+    public RoleServiceImpl(RoleRepository roleRepository, UserService userService) {
+        this.roleRepository = roleRepository;
+        this.userService = userService;
+    }
+
+    @Override
+    public Role create(Role role) {
+        Assert.notNull(role, "Role cannot be null");
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role readById(long id) {
+        return roleRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Role with Id " + id + " not found"));
+    }
+
+    @Override
+    public Role update(Role updatedRole) {
+        Assert.notNull(updatedRole, "Role cannot be null");
+        readById(updatedRole.getId());
+        return roleRepository.save(updatedRole);
+    }
+
+    @Override
+    public void delete(long id) {
+        readById(id);
+        roleRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Role> getAll() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public List<Role> findAllByUser(long userId) {
+        userService.readById(userId);
+        return roleRepository.findAllByUser(userId);
+    }
+}
