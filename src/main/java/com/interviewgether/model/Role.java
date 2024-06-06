e3c757580b41fae329c035f928c81e66c1fae77c
avhhust <avgystunpolek@gmail.com>
2024-06-06 18:30:43 -0500

    Created User and Role entities along with UserRepository and RoleRepository interfaces

---
+++ src/main/java/com/interviewgether/model/Role.java
@@ -0,0 +1,70 @@
+package com.interviewgether.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private long id;
+
+    @NotBlank
+    @Column(unique = true, name = "role_name")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id && Objects.equals(roleName, role.roleName) && Objects.equals(users, role.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName, users);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                ", users=" + users +
+                '}';
+    }
+}
