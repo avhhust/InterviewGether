c4d139f060d64d1a5c5827385f1cab0a71ec4b38
avhhust <avgystunpolek@gmail.com>
2024-06-19 20:42:11 -0500

    Added JpaUserDetailsService that implements UserDetailsService

---
+++ src/main/java/com/interviewgether/security/service/JpaUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.interviewgether.security.service;
+
+import com.interviewgether.model.User;
+import com.interviewgether.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public JpaUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameWithRoles(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
+        return new AuthUser(user);
+    }
+
+
+
+}
