80f67298ad6ca21e7866ed1d5bc200ae3a0c7949
avhhust <avgystunpolek@gmail.com>
2024-07-01 22:50:49 -0500

    Added security configuration for authServer

--- resource_server/src/main/java/com/interviewgether/config/ResourceServerSecurityConfig.java
+++ resource_server/src/main/java/com/interviewgether/config/ResourceServerSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +14,7 @@ public class ResourceServerSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                // configuration
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
                 .build();
     }
 
